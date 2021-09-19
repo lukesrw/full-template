cb86c0243fe4822cfe9559675cd6ed14644c1c86
lukesrw <lukesrw+github@gmail.com>
2021-09-19 12:16:40 +0100

    Moved package scripts into js

--- tools/init.js
+++ tools/init.js
@@ -7,13 +7,51 @@ function exec(command) {
 }
 
 module.exports = async () => {
-    await exec("npm run clone");
+    let names = process.argv.map(input => input.toLowerCase());
+    let is_test = names[2] === "--test";
 
-    for (let i = 2; i < process.argv.length; i += 1) {
-        await exec(`npm run ${process.argv[i]}-init`);
+    if (is_test) {
+        await exec("xcopy /E /I /Y ..\\full-template full-template");
+    } else {
+        await exec(
+            "git clone --depth=1 https://github.com/lukesrw/full-template.git"
+        );
     }
 
-    await exec("npm run clone-clean");
+    for (let i = 2; i < process.argv.length; i += 1) {
+        let name = process.argv[i].toLowerCase();
+
+        switch (name) {
+            case "clone":
+                'xcopy /E /I /Y . ..\\template && cd ..\\template && rmdir /S /Q eslint git prettier scss ts vue && git add -A && git commit -m "Full Template updates" && git push';
+                break;
+
+            case "ts":
+                await exec(
+                    "npm i -g typescript && npm i -D @types/node && move full-template\\ts ts"
+                );
+                break;
+
+            case "vue":
+                await exec(
+                    "npm i -g @vue/cli-service && npm i -g vue-template-compiler && npm i vue && npm i vue-router && move full-template\\vue vue"
+                );
+                break;
+
+            case "scss":
+            case "sass":
+                await exec("npm i -g sass && move full-template\\scss scss");
+                break;
+
+            case "electron":
+                await exec("move full-template\\electron .");
+
+            default:
+                await exec(`move full-template\\${name}\\* .`);
+        }
+    }
+
+    await exec("rmdir /S /Q full-template");
 };
 
 if (!module.parent) module.exports();
